984be535930a6a793399bc75c2d4a3584885300a
Niklas Dahlqvist <dalkmania@gmail.com>
2022-12-16 21:46:52 +0100

    Style Tweaks

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,16 +7,16 @@ const Footer = () => {
 
     return (
         <footer className="flex w-full justify-between items-center">
-            <p className="copyright"> &copy; {today.getFullYear()} Niklas Dahlqvist</p>
+            <p className="copyright text-sm md:text-base"> &copy; {today.getFullYear()} Niklas Dahlqvist</p>
             <div>
                 <button
-                    className="rounded-xl border-2 uppercase px-6 py-2 font-bold hover:cursor-pointer hover:animate-pulseScale mr-4"
+                    className="rounded-xl border-2 uppercase px-6 py-2 font-bold hover:cursor-pointer hover:animate-pulseScale mr-2 md:mr-4 text-sm md:text-base"
                     onClick={() => setPlayerScore(0)}
                 >
                     Reset Score
                 </button>
                 <button
-                    className="rounded-xl border-2 uppercase px-6 py-2 font-bold hover:cursor-pointer hover:animate-pulseScale"
+                    className="rounded-xl border-2 uppercase px-6 py-2 font-bold hover:cursor-pointer hover:animate-pulseScale text-sm md:text-base"
                     onClick={() => toggleRulesPopup(true)}
                 >
                     Rules
--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -9,7 +9,7 @@ const Popup = () => {
 
     return (
         <div
-            className={`absolute flex flex-col items-center justify-evenly  min-w-[30rem] min-h-[30rem] bg-white -m-6 translate-y-24 shadow-2xl ${toggleClass}`}
+            className={`absolute flex flex-col items-center justify-evenly  min-w-[30rem] min-h-[30rem] bg-white -m-6 translate-y-24 shadow-2xl rounded-xl ${toggleClass}`}
         >
             <h1 className="text-center text-dark-text uppercase text-3xl">Rules</h1>
             <img src={RulesImage} alt="Rules Image" />
