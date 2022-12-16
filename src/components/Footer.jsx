a67e13b89ef0afb21131c7d7a7ff3778692825bd
Niklas Dahlqvist <dalkmania@gmail.com>
2022-12-16 15:40:53 +0100

    Implemented Local Storage

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,17 +2,26 @@ import React, { useContext } from "react";
 import { GlobalContext } from "../context/GlobalState";
 
 const Footer = () => {
-    const { toggleRulesPopup } = useContext(GlobalContext);
+    const { toggleRulesPopup, setPlayerScore } = useContext(GlobalContext);
     const today = new Date();
+
     return (
         <footer className="flex w-full justify-between items-center">
             <p className="copyright"> &copy; {today.getFullYear()} Niklas Dahlqvist</p>
-            <button
-                className="rounded-xl border-2 uppercase px-6 py-2 font-bold hover:cursor-pointer hover:animate-pulseScale"
-                onClick={() => toggleRulesPopup(true)}
-            >
-                Rules
-            </button>
+            <div>
+                <button
+                    className="rounded-xl border-2 uppercase px-6 py-2 font-bold hover:cursor-pointer hover:animate-pulseScale mr-4"
+                    onClick={() => setPlayerScore(0)}
+                >
+                    Reset Score
+                </button>
+                <button
+                    className="rounded-xl border-2 uppercase px-6 py-2 font-bold hover:cursor-pointer hover:animate-pulseScale"
+                    onClick={() => toggleRulesPopup(true)}
+                >
+                    Rules
+                </button>
+            </div>
         </footer>
     );
 };
--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -1,8 +1,19 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { GlobalContext } from "../context/GlobalState";
+import { useLocalStorage } from "../hooks/useLocalStorage";
 
 const Score = () => {
-    const { playerScore } = useContext(GlobalContext);
+    const [score, setScore] = useLocalStorage("score", 0);
+    const { playerScore, useStorage, setPlayerScore } = useContext(GlobalContext);
+
+    useEffect(() => {
+        setPlayerScore(score);
+    }, []);
+
+    useEffect(() => {
+        setScore(playerScore);
+    }, [playerScore]);
+
     return (
         <div className="h-full bg-white p-2 text-score-text rounded-md min-w-[5rem] text-center uppercase font-bold text-sm shadow-md">
             Score
