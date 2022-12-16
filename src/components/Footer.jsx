d4e5626188523d5dd2ddc759f496424489c580d0
Niklas Dahlqvist <dalkmania@gmail.com>
2022-12-16 21:49:01 +0100

    Style Tweaks

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,13 +10,13 @@ const Footer = () => {
             <p className="copyright text-sm md:text-base"> &copy; {today.getFullYear()} Niklas Dahlqvist</p>
             <div>
                 <button
-                    className="rounded-xl border-2 uppercase px-6 py-2 font-bold hover:cursor-pointer hover:animate-pulseScale mr-2 md:mr-4 text-sm md:text-base"
+                    className="rounded-xl border-2 uppercase px-6 py-2 font-bold hover:cursor-pointer hover:animate-pulseScale mr-2 md:mr-4 text-xs md:text-base"
                     onClick={() => setPlayerScore(0)}
                 >
                     Reset Score
                 </button>
                 <button
-                    className="rounded-xl border-2 uppercase px-6 py-2 font-bold hover:cursor-pointer hover:animate-pulseScale text-sm md:text-base"
+                    className="rounded-xl border-2 uppercase px-6 py-2 font-bold hover:cursor-pointer hover:animate-pulseScale text-xs md:text-base"
                     onClick={() => toggleRulesPopup(true)}
                 >
                     Rules
